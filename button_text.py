a785bcf3e335f159e4326b27f2dff99fc104d8cc
Mike Bidinger <114107131+MikeBidinger@users.noreply.github.com>
2023-01-12 11:53:00 +0100

    Update button_text.py

--- button_text.py
+++ button_text.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame.locals import *
 
 # Define colours
 BG_COLOR = (204, 102, 0)
@@ -14,30 +13,6 @@ DEF_BUTTON = (200, 200, 200)
 counter = 0
 
 
-def invert_rgb(rgb_val):
-    r_val, g_val, b_val = rgb_val
-    r_val = 255 - r_val
-    g_val = 255 - g_val
-    b_val = 255 - b_val
-    return (r_val, g_val, b_val)
-
-
-def darken_rgb(rgb_val, ratio=2, steps=4):
-    r_val, g_val, b_val = rgb_val
-    r_val = r_val - int(ratio * (r_val / steps))
-    g_val = g_val - int(ratio * (g_val / steps))
-    b_val = b_val - int(ratio * (b_val / steps))
-    return (r_val, g_val, b_val)
-
-
-def lighten_rgb(rgb_val, ratio=2, steps=4):
-    r_val, g_val, b_val = rgb_val
-    r_val = int(ratio * ((r_val + 255) / steps))
-    g_val = int(ratio * ((g_val + 255) / steps))
-    b_val = int(ratio * ((b_val + 255) / steps))
-    return (r_val, g_val, b_val)
-
-
 class States():
     def __init__(self):
         self.normal = 0
@@ -45,102 +20,111 @@ class States():
         self.pressed = 2
 
 
-button_states = States()
+BUTTON_STATES = States()
+
+
+def invert_rgb(rgb_val):
+    r_val = 255 - rgb_val[0]
+    g_val = 255 - rgb_val[1]
+    b_val = 255 - rgb_val[2]
+    return (r_val, g_val, b_val)
+
+
+def darken_rgb(rgb_val, ratio=2, steps=4):
+    r_val = rgb_val[0] - int(ratio * (rgb_val[0] / steps))
+    g_val = rgb_val[1] - int(ratio * (rgb_val[1] / steps))
+    b_val = rgb_val[2] - int(ratio * (rgb_val[2] / steps))
+    return (r_val, g_val, b_val)
+
+
+def lighten_rgb(rgb_val, ratio=2, steps=4):
+    r_val = rgb_val[0] + int(ratio * ((rgb_val[0] + 255) / steps))
+    g_val = rgb_val[1] + int(ratio * ((rgb_val[1] + 255) / steps))
+    b_val = rgb_val[2] + int(ratio * ((rgb_val[2] + 255) / steps))
+    return (r_val, g_val, b_val)
 
 
 class Button():
-    def __init__(self, width, height, x, y, value, font,
-                 btn_col=DEF_BUTTON, border_px=1):
+    def __init__(self, width, height, x, y, text, font, value, 
+                 btn_col=DEF_BUTTON, border_px=2, shading=True):
         # Rectangle
         self.rect = pygame.Rect(x, y, width, height)
         # State
-        self.state = button_states.normal
-        # Button
+        self.state = BUTTON_STATES.normal
+        # Text
+        self.text = text
+        self.font = font
+        # Value
         self.value = value
-        self.cur_btn_col = btn_col
-        self.btn_col = btn_col
+        # Colors
+        self.button_col = btn_col
         self.darken_effect = False
-        for i in btn_col:
+        for i in self.button_col:
             if i > 128:
                 self.darken_effect = True
                 break
-        self.hover_col = self.btn_col
-        self.press_col = self.btn_col
-        # Text
-        self.font = font
-        self.txt_col = BLACK
-        self.text = self.font.render(value, True, self.txt_col)
+        if self.darken_effect:
+            self.text_col = (0, 0, 0)
+            self.border_color = (0, 0, 0)
+            self.hover_col = darken_rgb(self.button_col)
+            self.press_col = darken_rgb(self.button_col, 3)
+        else:
+            self.text_col = invert_rgb((0, 0, 0))
+            self.border_color = invert_rgb((0, 0, 0))
+            self.hover_col = lighten_rgb(self.button_col)
+            self.press_col = lighten_rgb(self.button_col, 3)
         # Border (shading)
-        self.border_col = BLACK
         self.border_thickness = border_px
+        self.shading = shading
 
     def draw_button(self, surface):
-        # Draw button
-        pygame.draw.rect(surface, self.cur_btn_col, self.rect)
-        # Draw text
-        width = self.text.get_width()
-        height = self.text.get_height()
-        # Draw borders
-        offset = int((self.border_thickness / 3) + (self.border_thickness / 5))
-        x = self.rect.x + offset
-        y = self.rect.y + offset
-        top_left = (x, y)
-        top_right = (x + self.rect.width - self.border_thickness, y)
-        bottom_left = (x, y + self.rect.height - self.border_thickness)
-        bottom_right = (x + self.rect.width - self.border_thickness,
-                        y + self.rect.height - self.border_thickness)
-        pygame.draw.line(surface, self.border_col,  # Top
-                         top_left, top_right, self.border_thickness)
-        pygame.draw.line(surface, self.border_col,  # Left
-                         top_left, bottom_left, self.border_thickness)
-        pygame.draw.line(surface, self.border_col,  # Bottom
-                         bottom_left, bottom_right, self.border_thickness)
-        pygame.draw.line(surface, self.border_col,  # Right
-                         top_right, bottom_right, self.border_thickness)
-        # Calculate position
-        pos_x = self.rect.centerx - (width / 2)
-        pos_y = self.rect.centery - (height / 2)
-        # Draw button onto surface
-        surface.blit(self.text, (pos_x, pos_y))
-
-    def is_clicked(self, event):
-        if self.is_collide():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                self.press = True
-                self.change_color(button_states.pressed)
-                return True
-            else:
-                return False
-        else:
-            return False
-
-    def is_collide(self):
+        action = False
+        # Get mouse position
         pos = pygame.mouse.get_pos()
+        # Set button state
         if self.rect.collidepoint(pos):
-            self.change_color(button_states.hover)
-            return True
+            if pygame.mouse.get_pressed()[0] == 1:
+                self.state = BUTTON_STATES.pressed
+            elif pygame.mouse.get_pressed()[0] == 0 and self.state == BUTTON_STATES.pressed:
+                self.state = BUTTON_STATES.hover
+                action = True
+            else:
+                self.state = BUTTON_STATES.hover
         else:
-            self.change_color(button_states.normal)
-            return False
-
-    def is_pressed(self):
-        self.change_color(button_states.pressed)
-
-    def change_color(self, state: States):
-        if state == button_states.normal:
-            self.cur_btn_col = self.btn_col
-        if self.darken_effect:
-            self.border_col = (0, 0, 0)
-            self.txt_col = (0, 0, 0)
-            if state == button_states.hover:
-                self.cur_btn_col = darken_rgb(self.btn_col)
-            elif state == button_states.pressed:
-                self.cur_btn_col = darken_rgb(self.btn_col, 3)
+            self.state = BUTTON_STATES.normal
+        # Draw button (state dependent)
+        if self.state == BUTTON_STATES.normal:
+            pygame.draw.rect(surface, self.button_col, self.rect)
+        elif self.state == BUTTON_STATES.hover:
+            pygame.draw.rect(surface, self.hover_col, self.rect)
+        elif self.state == BUTTON_STATES.pressed:
+            pygame.draw.rect(surface, self.press_col, self.rect)
+        # Draw border (state and shading dependent)
+        if self.shading:
+            top_left = WHITE
+            bottom_right = BLACK
         else:
-            self.border_col = invert_rgb((0, 0, 0))
-            self.txt_col = invert_rgb((0, 0, 0))
-            if state == button_states.hover:
-                self.cur_btn_col = lighten_rgb(self.btn_col)
-            elif state == button_states.pressed:
-                self.cur_btn_col = lighten_rgb(self.btn_col, 3)
-        self.text = self.font.render(self.value, True, self.txt_col)
+            top_left = self.border_color
+            bottom_right = self.border_color
+        if self.state == BUTTON_STATES.hover or self.state == BUTTON_STATES.pressed:
+            top_left = invert_rgb(top_left)
+            bottom_right = invert_rgb(bottom_right)
+        pygame.draw.line(surface, top_left, (self.rect.x, self.rect.y),
+                         (self.rect.x + self.rect.width, self.rect.y), self.border_thickness)
+        pygame.draw.line(surface, top_left, (self.rect.x, self.rect.y),
+                         (self.rect.x, self.rect.y + self.rect.height), self.border_thickness)
+        pygame.draw.line(surface, bottom_right, (self.rect.x, self.rect.y + self.rect.height),
+                         (self.rect.x + self.rect.width, self.rect.y + self.rect.height), self.border_thickness)
+        pygame.draw.line(surface, bottom_right, (self.rect.x + self.rect.width, self.rect.y),
+                         (self.rect.x + self.rect.width, self.rect.y + self.rect.height), self.border_thickness)
+        # Draw text (state dependent)
+        col_val = self.text_col
+        if self.state == BUTTON_STATES.hover or self.state == BUTTON_STATES.pressed:
+            col_val = invert_rgb(self.text_col)
+        text_img = self.font.render(self.text, True, col_val)
+        text_rect = text_img.get_rect()
+        text_rect.center = ((self.rect.x + (self.rect.width // 2),
+                            self.rect.y + (self.rect.height // 2)))
+        surface.blit(text_img, text_rect)
+        # Return click action
+        return action
